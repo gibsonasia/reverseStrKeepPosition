5cfa6b33fa2196de1448798c7c662e05be018f6d
Asia Gibson <gibsonjasia@gmail.com>
2022-01-25 11:26:50 -0500

    fixed logical error
    Had a logical error in first commit now its fixed

--- reverseStrWords.js
+++ reverseStrWords.js
@@ -1,3 +1,30 @@
+//working function updated with fixed logical error
+    
+function reverseStrWords(str){
+
+  let myArr = str.split(" "); // puts each work from string into an array
+  let tempArr = []; //new array with words reversed 
+  let tempWord =""; //will hold the string thats reversed
+   //place holder for the index of the current string index
+
+  //LOOPS through array from end
+  for(let i = 0; i < myArr.length; i++){
+
+    var word = myArr[i]; //holds word at current index
+
+//Loops through each char of the word from the end
+    for(let j = word.length - 1; j >= 0; j-- ){
+      //holds word at current index
+      tempWord += word.charAt(j); //builds reverse string 
+          
+    }
+    
+  }
+    tempArr.push(tempWord + " "); //adds revered string to array 
+    return tempArr.join(" "); //turns the array back into a string
+
+  //function below had logical error 
+/*
 function reverseStrWords(str){
 
   let myArr = str.split(""); // puts each work from string into an array
@@ -20,6 +47,6 @@ function reverseStrWords(str){
   }
     tempArr.push(tempWord); //adds revered string to array 
     return tempArr.join(""); //turns the array back into a strind
-}
+} */
 
 //Definetly can create a func with better runtime
